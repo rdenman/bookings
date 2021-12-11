0a4451a5688eb78b1f2683ae91a256f381ccf1b0
Rob Denman <robertkarldenman@gmail.com>
2021-12-11 09:20:45 -0600

    add some tests

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Bookings and Reservations
 
 A bookings & reservations app built with Go.
+
+## Start the App
+
+Run `go run cmd/web/*.go`
+
+## Test with Coverage
+
+Run `go test -coverprofile=coverage.out && go tool cover -html=coverage.out`
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,6 +20,24 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	srv := &http.Server {
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	gob.Register(models.Reservation{})
 
 	app.InProduction = false
@@ -34,7 +52,7 @@ func main() {
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -45,14 +63,5 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	srv := &http.Server {
-		Addr: portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return nil
 }
